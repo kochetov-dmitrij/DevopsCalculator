e75c355a03ba169cfa8a9ebf3a8c95d9bf5460f3
kochetov-dmitrij <dimak.o@mail.ru>
2020-03-19 20:13:24 +0300

    db works

--- src/main/java/com/example/calculator/CalculateController.java
+++ src/main/java/com/example/calculator/CalculateController.java
@@ -32,6 +32,7 @@ public class CalculateController extends ParameterizableViewController {
     private LinkedList<String> history = new LinkedList<>();
     private int historyCounter = 1;
     private Calculator calculator;
+    private Connection db;
 
     @Override
     protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -71,23 +72,87 @@ public class CalculateController extends ParameterizableViewController {
 
     public void setCalculator(Calculator calculator) {
         this.calculator = calculator;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.print("org.postgresql.Driver not found");
+            return;
+        }
+        try {
+            db = DriverManager.getConnection(
+                    "jdbc:postgresql:postgres",
+                    "admin",
+                    "pass");
+        } catch (SQLException e) {
+            System.out.print("SQL exception\n" + e.getMessage());
+        }
     }
 
     private String wrapRecord(String equation) {
         return String.format("<div class=\"record\">%s</div>", equation);
     }
 
-    private void appendRecord(String equation) {
-        if (equation == null) return;
-        if (history.size() == 5) history.removeLast();
-        history.add(0, String.format("%d) %s", historyCounter++, equation));
-    }
+//    private void appendRecord(String equation) {
+//        if (equation == null) return;
+//        if (history.size() == 5) history.removeLast();
+//        history.add(0, String.format("%d) %s", historyCounter++, equation));
+//    }
+//
+//    private String getHistory() {
+//        StringBuilder sb = new StringBuilder();
+//        for (String s : history) {
+//            sb.append(wrapRecord(s));
+//        }
+//        return sb.toString();
+//    }
 
     private String getHistory() {
-        StringBuilder sb = new StringBuilder();
-        for (String s : history) {
-            sb.append(wrapRecord(s));
+        try {
+            PreparedStatement pst = db.prepareStatement("SELECT record FROM history ORDER BY ts DESC");
+            ResultSet rs = pst.executeQuery();
+            StringBuilder sb = new StringBuilder();
+
+            while (rs.next()) {
+                sb.append(wrapRecord(rs.getString(1)));
+            }
+            return sb.toString();
+        } catch (SQLException e) {
+            System.out.print("SQL exception. Could not append.\n" + e.getMessage());
+            return "";
         }
-        return sb.toString();
+    }
+
+    private void appendRecord(String equation) {
+        if (equation == null) return;
+
+        try {
+            PreparedStatement pst;
+            ResultSet rs;
+
+            pst = db.prepareStatement("SELECT COUNT(*) FROM history");
+            rs = pst.executeQuery();
+            rs.next();
+            int length = rs.getInt(1);
+
+            if (length == 5) {
+                pst = db.prepareStatement("SELECT ts FROM history ORDER BY ts ASC LIMIT 1");
+                rs = pst.executeQuery();
+                rs.next();
+                Timestamp ts = rs.getTimestamp(1);
+
+                pst = db.prepareStatement("DELETE FROM history WHERE ts = (?)");
+                pst.setTimestamp(1, ts);
+                pst.executeUpdate();
+            }
+
+            pst = db.prepareStatement("INSERT INTO history(ts, record) VALUES(?, ?)");
+            pst.setTimestamp(1, new Timestamp(System.currentTimeMillis()));
+            pst.setString(2, String.format("%d) %s", historyCounter++, equation));
+            pst.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.print("SQL exception. Could not append.\n" + e.getMessage());
+        }
     }
 }
--- src/main/webapp/calculator.jsp
+++ src/main/webapp/calculator.jsp
@@ -19,32 +19,32 @@
                     </td>
                 </tr>
                 <tr>
-                    <td><input type="submit" name="button" id="btn-7" value="7"/></td>
-                    <td><input type="submit" name="button" id="btn-8" value="8"/></td>
-                    <td><input type="submit" name="button" id="btn-9" value="9"/></td>
-                    <td><input type="submit" name="button" id="btn-/" value="/"/></td>
+                    <td><input class="btn" type="submit" name="button" id="btn-7" value="7"/></td>
+                    <td><input class="btn" type="submit" name="button" id="btn-8" value="8"/></td>
+                    <td><input class="btn" type="submit" name="button" id="btn-9" value="9"/></td>
+                    <td><input class="btn" type="submit" name="button" id="btn-/" value="/"/></td>
                     <td class="empty"></td>
-                    <td><input type="submit" name="button" id="btn-C" value="C"/></td>
+                    <td><input class="btn" type="submit" name="button" id="btn-C" value="C"/></td>
                 </tr>
                 <tr>
-                    <td><input type="submit" name="button" id="btn-4" value="4"/></td>
-                    <td><input type="submit" name="button" id="btn-5" value="5"/></td>
-                    <td><input type="submit" name="button" id="btn-6" value="6"/></td>
-                    <td><input type="submit" name="button" id="btn-*" value="*"/></td>
+                    <td><input class="btn" type="submit" name="button" id="btn-4" value="4"/></td>
+                    <td><input class="btn" type="submit" name="button" id="btn-5" value="5"/></td>
+                    <td><input class="btn" type="submit" name="button" id="btn-6" value="6"/></td>
+                    <td><input class="btn" type="submit" name="button" id="btn-*" value="*"/></td>
 
                 </tr>
                 <tr>
-                    <td><input type="submit" name="button" id="btn-1" value="1"/></td>
-                    <td><input type="submit" name="button" id="btn-2" value="2"/></td>
-                    <td><input type="submit" name="button" id="btn-3" value="3"/></td>
-                    <td><input type="submit" name="button" id="btn--" value="-"/></td>
+                    <td><input class="btn" type="submit" name="button" id="btn-1" value="1"/></td>
+                    <td><input class="btn" type="submit" name="button" id="btn-2" value="2"/></td>
+                    <td><input class="btn" type="submit" name="button" id="btn-3" value="3"/></td>
+                    <td><input class="btn" type="submit" name="button" id="btn--" value="-"/></td>
                     <td class="empty"></td>
-                    <td rowspan="2"><input class="double_height" type="submit" name="button" id="btn-=" value="="/>
+                    <td rowspan="2"><input class="btn double_height" type="submit" name="button" id="btn-=" value="="/>
                 </tr>
                 <tr>
-                    <td colspan="2"><input class="double_width" type="submit" name="button" id="btn-0" value="0"/></td>
-                    <td><input type="button" name="button" id="btn-." value="."/></td>
-                    <td><input type="submit" name="button" id="btn-+" value="+"/></td>
+                    <td colspan="2"><input class="btn double_width" type="submit" name="button" id="btn-0" value="0"/></td>
+                    <td><input class="btn" type="button" name="button" id="btn-." value="."/></td>
+                    <td><input class="btn" type="submit" name="button" id="btn-+" value="+"/></td>
                 </tr>
             </table>
         </form>

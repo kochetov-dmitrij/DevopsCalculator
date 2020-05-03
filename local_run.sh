0a786c5a6a8251a45181bde627bdbb1c6b038732
kochetov-dmitrij <dimak.o@mail.ru>
2020-05-04 01:31:53 +0300

    added test frameworks for coverage and mutations

--- local_run.sh
+++ local_run.sh
@@ -3,12 +3,11 @@ cp ~/.ssh/id_rsa ./environments/dev/
 cp ~/.ssh/id_rsa.pub ./environments/dev/
 
 label=calculator
-old_container=`docker ps -a | grep $label | awk '{print $1}'`
+old_container=$(docker ps -a | grep $label | awk '{print $1}')
 if [ ! -z "$old_container" ] ; then docker rm -f $old_container ; fi
 
 docker build -t $label ./environments/dev
-container_id=`docker run -d -v data:/temp/ -p 9024:22 -p 16000:8080 $label`
-container_ip=`docker inspect -f "{{ .NetworkSettings.IPAddress }}" $container_id`
+container_id=$(docker run -d -v data:/temp/ -p 9024:22 -p 16000:8080 $label)
 
 export ANSIBLE_HOST_KEY_CHECKING=False
 ansible-playbook -i local_inventory.yml -k ./environments/dev/playbook.yml
@@ -17,4 +16,4 @@ docker cp ./.mvn/ $container_id:tmp
 docker cp ./pom.xml $container_id:tmp
 docker cp ./src/ $container_id:tmp
 
-docker exec $container_id bash -c 'cd /tmp/ ; ./mvnw clean package ; cp target/calculator-web-*.war /var/lib/tomcat8/webapps/ROOT.war ; ./mvnw failsafe:integration-test -DskipTests=false'
+docker exec $container_id bash -c 'cd /tmp/ ; ./mvnw clean verify ; ./mvnw org.pitest:pitest-maven:mutationCoverage ; cp target/calculator-web-*.war /var/lib/tomcat8/webapps/ROOT.war ; mkdir /var/lib/tomcat8/webapps/reports ; cp -r /tmp/target/site/jacoco/ /var/lib/tomcat8/webapps/reports/test-coverage ;  cp -r /tmp/target/pit-reports/*/ /var/lib/tomcat8/webapps/reports/mutation ; ./mvnw failsafe:integration-test -DskipTests=false'
